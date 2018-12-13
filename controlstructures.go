7feb3a94914505f8b106da84470d863059993792
Ephraim Etuk <35599663+E-phraim@users.noreply.github.com>
2018-12-13 12:56:27 +0100

    Add files via upload

---
+++ controlstructures.go
@@ -0,0 +1,45 @@
+// 1.
+package main
+
+import "fmt"
+func main() {
+	i := 10
+	if i > 10 {
+		fmt.Println("Big")
+	}	else {
+		fmt.Println("Small")
+	}
+}
+
+// Your answer should be "Small" because it's not bigger than 10
+
+// 2.
+
+package main
+import "fmt"
+
+func main () {
+	for i := 1; i <= 100; i++{
+		if i % 3 == 0{
+			fmt.Println(i)
+		}
+	}
+}
+
+// 3.
+package main
+import "fmt"
+
+func main () {
+	for i := 1; i <= 100; i++{
+		if i % 15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
