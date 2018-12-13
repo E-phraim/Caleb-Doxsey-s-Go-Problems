3806ff3d1dd02936e9610b470e536e5ab240bf0a
Ephraim Etuk <35599663+E-phraim@users.noreply.github.com>
2018-12-13 10:40:12 +0100

    Add files via upload

---
+++ Your First Program.go
@@ -0,0 +1,27 @@
+1. White-space is also known as newlines, spaces and tabs and 
+also these White-spaces are not actually put into consideration 
+when compiling, they are just there to make our programms look neat.
+
+2.A comment is a programmer-readable explanation or annotation in the 
+source code of a computer program. They are added with the purpose of 
+making the source code easier for humans to understand, and are generally 
+ignored by compilers and interpreters .
+The two ways of writing comments in Go are; // (this type of comment applies on just one line)
+											/* (everthing comes in here and
+											 spans numbers of
+											  lines if 
+											  need
+											   be)  */ 
+
+3. the "fmt" package would begin with the "import" statement.
+
+4. we would have to import the "os" package as well
+
+5.   
+
+package main()
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, my name is E-phraim")
+}
